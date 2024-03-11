37ef3369c33f6fafa9a0244471b301c913c17ada
Paul Dowman <paul@pauldowman.com>
2024-03-11 16:40:57 -0600

    Add info and link to the repo.

--- web/src/App.js
+++ web/src/App.js
@@ -10,6 +10,9 @@ function App() {
         // registryUrl="http://localhost:3000/registry.json"
         // ipfsGateway="http://localhost:8080/ipfs/"
       />
+      <div className="info">
+        <p>This verifier is developed by <a href="https://www.eqtylab.io/">EQTY Lab</a> and the code is <a href="https://github.com/eqtylab/risc-zero-verifier">available on GitHub</a>.</p>
+      </div>
     </div>
   );
 }
